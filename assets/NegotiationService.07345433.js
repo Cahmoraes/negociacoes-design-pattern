fb08a70399dcd396e15be3a59063449ed7d48a33
caique.moraes <caique.moraes@compasso.com.br>
2022-11-22 12:01:25 -0300

    Updates

--- assets/NegotiationService.07345433.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index.c491fd5f.js";class s{constructor(t){this.service=t}async get(){return e.Maybe.of(await this.service.get())}}export{s as NegotiationService};
---
+++ assets/NegotiationService.353da97c.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index.1937e8c8.js";class s{constructor(t){this.service=t}async get(){return e.Maybe.of(await this.service.get())}}export{s as NegotiationService};
